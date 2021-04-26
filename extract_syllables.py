8130678716567881b16cade27d4a6f591cb66067
Julian Buechel <jubueche@ethz.ch>
2021-04-26 17:09:21 +0200

    Added CNN training

--- extract_syllables.py
+++ extract_syllables.py
@@ -6,9 +6,9 @@ from scipy.signal import find_peaks
 from utils import *
 from joblib import dump, load
 
-def predict_syllables(X,idx_spectogram,clf,wnd_sz):
+def predict_syllables_SVM(X,idx_spectogram,clf,wnd_sz):
     """
-    Take 128 x T sized spectogram and predict the on and off of syllables
+    Take 128 x T sized spectogram and predict the on and off of syllables using SVM
     """
     w = int(wnd_sz / 2)
     is_on = w * [0]
@@ -18,6 +18,27 @@ def predict_syllables(X,idx_spectogram,clf,wnd_sz):
         X_s.append(X_f)
     X_s = np.array(X_s)
     y_pred = clf.predict(X_s)
+    is_on.extend(y_pred)
+    is_on.extend(w*[0])
+    
+    _, peaks_dict = find_peaks(is_on, plateau_size=[5,200])
+    le = peaks_dict['left_edges']
+    re = peaks_dict['right_edges']
+    return_tuples = [(a,b,idx_spectogram) for a,b in zip(le,re)]
+    return return_tuples
+
+def predict_syllables_CNN(X,idx_spectogram,cnn,wnd_sz):
+    """
+    Take 128 x T sized spectogram and predict the on and off of syllables using CNN
+    """
+    w = int(wnd_sz / 2)
+    is_on = w * [0]
+    X_s = []
+    for t in range(w,X.shape[1]-w):
+        X_f = X[:,t-w:t+w]
+        X_s.append(X_f)
+    X_s = np.array(X_s)
+    y_pred = cnn(X_s)
     is_on.extend(y_pred)
     is_on.extend(w*[0])
     
@@ -45,19 +66,58 @@ def train_syllable_extractor():
     model_path = path.join(base,"Data/syllable_extr.model")
     wnd_sz = 10
     if not path.isfile(model_path):
-        X_train, y_train = get_train_data(Xs_train, ground_truth_tuples, wnd_sz=wnd_sz, dt=5, limit=200000)
-        clf = train_model(X_train,y_train)
+        X_train, y_train = get_train_data(Xs_train, ground_truth_tuples, wnd_sz=wnd_sz, dt=5, limit=70000)
+        clf = train_SVM(X_train,y_train)
         dump(clf, model_path) 
     else:
         clf = load(model_path)
 
     def syllable_extractor(spectogram, idx):
-        return predict_syllables(spectogram, idx, clf, wnd_sz=wnd_sz)
+        return predict_syllables_SVM(spectogram, idx, clf, wnd_sz=wnd_sz)
 
     return syllable_extractor
 
+def extract_and_store_datasets(wnd_sizes, use_feature_extraction, limit):
+    """
+    For every wnd size, extract a dataset from the training data with or without extracted features.
+    """
+    base = path.dirname(path.abspath(__file__))
+    if not os.path.isdir(os.path.join(base,"Data/Datasets")):
+        os.mkdir(os.path.join(base,"Data/Datasets"))
 
-def evaluate():
+    download()
+    # - Get the training data
+    Xs_train = loadmat(path.join(base,"Data/train_dataset/spectrograms/g17y2_train.mat"))["Xs_train"][0]
+    annotations = loadmat(path.join(base,"Data/train_dataset/annotations/annotations_g17y2_train.mat"))["annotations_train"][0]
+
+    SETindex = annotations["y"][0]["SETindex"][0][0][0]-1
+    ons = annotations["y"][0]["onset_columns"][0][0][0]-1
+    offs = annotations["y"][0]["offset_columns"][0][0][0]-1
+    ground_truth_tuples = [(a,b,c) for a,b,c in zip(ons,offs,SETindex)]
+
+    for wnd_sz in wnd_sizes:
+        file_name = ("_features_%s_wnd_sz_%s_limit_%s.npy" % (str(use_feature_extraction),str(wnd_sz),str(limit)))
+        if not os.path.isfile(os.path.join(base,"Data/Datasets/"+"X"+file_name)):
+            X_train, y_train = get_train_data(Xs_train, ground_truth_tuples, wnd_sz=wnd_sz, dt=5, limit=limit, use_features=use_feature_extraction)
+            np.save(os.path.join(base,"Data/Datasets/"+"X"+file_name), X_train, allow_pickle=True)
+            np.save(os.path.join(base,"Data/Datasets/"+"y"+file_name), y_train, allow_pickle=True)
+
+def load_dataset(wnd_sz, use_feature_extraction, limit):
+    """
+    Load the dataset and return X and y
+    """
+    base = path.dirname(path.abspath(__file__))
+    file_name = ("_features_%s_wnd_sz_%s_limit_%s.npy" % (str(use_feature_extraction),str(wnd_sz),str(limit)))
+    if os.path.isfile(os.path.join(base, "Data/Datasets/" + "X" + file_name)):
+        X = np.load(os.path.join(base, "Data/Datasets/" + "X" + file_name), allow_pickle=True)
+        y = np.load(os.path.join(base, "Data/Datasets/" + "y" + file_name), allow_pickle=True)
+        return X,y
+    else:
+        print("WARNING: Dataset not found.")
+        return None
+
+
+def evaluate(syllable_extractor = None):
     base = path.dirname(path.abspath(__file__))
     download()
     # - Get the training data
@@ -69,7 +129,8 @@ def evaluate():
     offs = annotations["y"][0]["offset_columns"][0][0][0]-1
     ground_truth_tuples = [(a,b,c) for a,b,c in zip(ons,offs,SETindex)]
 
-    syllable_extractor = train_syllable_extractor()
+    if syllable_extractor is None:
+        syllable_extractor = train_syllable_extractor()
 
     # - Generate figure for the report
     fig = plt.figure(figsize=(14,6))
@@ -88,6 +149,9 @@ def evaluate():
         os.mkdir(path.join(base,"Data/Figures"))
     plt.savefig(path.join(base,"Data/Figures/syllable_detection.svg"), format='svg')
     plt.show()
+
+    # - Uncomment to run through
+    # # return
 
     predicted_tuples = []
     correct_length = []
@@ -114,5 +178,18 @@ def evaluate():
 
 
 if __name__ == "__main__":
-    evaluate()
+    # evaluate()
+    use_feature_extraction = False
+    wnd_sz = 20
+    limit = 70000
 
+    extract_and_store_datasets([20,28,64,104], use_feature_extraction=use_feature_extraction, limit=limit)
+
+    X,y = load_dataset(wnd_sz=wnd_sz, use_feature_extraction=use_feature_extraction, limit=limit)
+
+    cnn = train_CNN(X,y,("CNN_features_%s_wnd_sz_%s_limit_%s.model" % (use_feature_extraction,wnd_sz,limit)))
+
+    def syllable_extractor_CNN(spectogram, idx):
+        return predict_syllables_CNN(spectogram, idx, cnn, wnd_sz=wnd_sz)
+
+    evaluate(syllable_extractor=syllable_extractor_CNN)
--- utils.py
+++ utils.py
@@ -9,6 +9,14 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.utils.data import TensorDataset, DataLoader
+
+# - Set device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
 def reverse_on_off(tuples,T):
     reversed_tuples =  []
     next_on = 0
@@ -22,7 +30,7 @@ def reverse_on_off(tuples,T):
     return reversed_tuples
             
 
-def get_train_data(spectograms, ground_truth_tuples, wnd_sz, limit=10000, dt=3):
+def get_train_data(spectograms, ground_truth_tuples, wnd_sz, limit=10000, dt=3, use_features=True):
     """
     Extract 128 x wnd_sz chunks from within and outside [on,off] and store in list.
     Extract features from the list and store in array. Return array.
@@ -37,7 +45,10 @@ def get_train_data(spectograms, ground_truth_tuples, wnd_sz, limit=10000, dt=3):
         r = int(t+w)
         if l < 0 or r >= spectograms[idx].shape[1]:
             return None
-        X_tmp = extract_features(spectograms[idx][:,l:r])
+        if use_features:
+            X_tmp = extract_features(spectograms[idx][:,l:r])
+        else:
+            X_tmp = spectograms[idx][:,l:r]
         return X_tmp
 
     for idx_tuple,tup in enumerate(ground_truth_tuples):
@@ -77,7 +88,7 @@ def get_train_data(spectograms, ground_truth_tuples, wnd_sz, limit=10000, dt=3):
     ys = np.array(ys)
     return Xs[:limit],ys[:limit]
 
-def train_model(X,y):
+def train_SVM(X,y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
     clf = make_pipeline(StandardScaler(), SVC(gamma='auto'))
     clf.fit(X_train, y_train)
@@ -117,3 +128,153 @@ def download():
         os.system("gdown https://drive.google.com/uc?id=1nvLex6f5HY48eZ6MyvVcP122Q5N9sDr2")
         os.system("mv train_dataset.zip Data/")
         os.system("unzip Data/train_dataset.zip -d Data/")
+
+def normalize(X, mean=None, std=None):
+    if not (mean == None):
+        return (X - mean) / std
+    else:
+        return (X - torch.mean(X)) / torch.std(X)
+
+class BirdDataLoader:
+
+    def __init__(self,X,y):
+        self.wnd_sz = X.shape[2]
+        # - Split data 
+        self.X_train, X_test, self.y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+        self.X_test, self.X_val, self.y_test, self.y_val = train_test_split(X_test, y_test, test_size=0.5, random_state=42)
+
+        self.mean = torch.mean(torch.tensor(self.X_train).float())
+        self.std = torch.std(torch.tensor(self.X_train).float())
+
+        self.train_dataset = TensorDataset(torch.reshape(normalize(torch.tensor(self.X_train).float()),(-1,1,128,self.wnd_sz)),torch.tensor(self.y_train))
+        self.val_dataset = TensorDataset(torch.reshape(normalize(torch.tensor(self.X_val).float()),(-1,1,128,self.wnd_sz)),torch.tensor(self.y_val))
+        self.test_dataset = TensorDataset(torch.reshape(normalize(torch.tensor(self.X_test).float()),(-1,1,128,self.wnd_sz)),torch.tensor(self.y_test))
+
+    def get_data_loader(self, dset, shuffle, num_workers, batch_size):
+        if dset == "train":
+            dataloader = DataLoader(dataset=self.train_dataset, shuffle=shuffle, num_workers=num_workers, batch_size=batch_size)
+        elif dset == "val":
+            dataloader = DataLoader(dataset=self.val_dataset, shuffle=shuffle, num_workers=num_workers, batch_size=batch_size)
+        elif dset == "test":
+            dataloader = DataLoader(dataset=self.test_dataset, shuffle=False, num_workers=num_workers, batch_size=batch_size)
+        else:
+            assert False, "Unknown dset"
+        return dataloader
+
+def train_CNN(X,y,model_name):
+    # - Create data loader
+    dataloader = BirdDataLoader(X,y)
+
+    # - Create data loader test
+    data_loader_test = dataloader.get_data_loader(
+        dset="test", shuffle=True, num_workers=4, batch_size=64
+    )
+
+    # - Figure out window size
+    wnd_sz = X.shape[2]
+
+    # - Set the seed
+    torch.manual_seed(42)
+
+    # - Setup path
+    base = path.dirname(path.abspath(__file__))
+    save_path = path.join(base,"Data/"+model_name)
+
+    cnn = load_cnn(save_path, wnd_sz)
+    best_val_acc = -np.inf
+    if cnn is None:
+        cnn = get_CNN_architecture(wnd_sz)
+        data_loader_train = dataloader.get_data_loader(
+            dset="train", shuffle=True, num_workers=4, batch_size=64
+        )
+        data_loader_val = dataloader.get_data_loader(
+            dset="val", shuffle=True, num_workers=4, batch_size=64
+        )
+        optim = torch.optim.Adam(cnn.parameters(), lr=1e-4)
+        n_epochs = 10
+        for n in range(n_epochs):
+            for idx, (data, target) in enumerate(data_loader_train):
+                data, target = data.to(device), target.to(device)  # GPU
+                output = cnn(data)
+                optim.zero_grad()
+                loss = F.cross_entropy(output, target)
+                if idx % 100 == 0:
+                    print(f"Epoch {n} Loss {float(loss)}")
+                loss.backward()
+                optim.step()
+
+            # - Do evaluation
+            val_acc = evaluate_model(cnn, data_loader_val)
+            print(f"Validation accuracy is {val_acc}")
+            if val_acc > best_val_acc:
+                best_val_acc = val_acc
+                torch.save(cnn.state_dict(), save_path)
+
+    test_acc = evaluate_model(cnn, data_loader_test)
+    print(f"Test acc. {test_acc}")
+    cnn.eval()
+
+    def model_wrapper(X):
+        X = torch.reshape(normalize(torch.tensor(X), mean=dataloader.mean, std=dataloader.std),(-1,1,128,wnd_sz)).to(device)
+        pred = cnn(X)
+        return torch.argmax(pred, dim=1)
+
+    return model_wrapper
+
+def evaluate_model(cnn, data_loader):
+    cnn.eval()
+    correct = 0.0; num = 0.0
+    for data,target in data_loader:
+        data, target = data.to(device), target.to(device)  # GPU
+        pred = cnn(data)
+        correct += torch.sum((torch.argmax(pred,dim=1) == target).float())
+        num += len(pred)
+    acc = float(correct / num)
+    cnn.train()
+    return acc
+
+def load_cnn(path, wnd_sz):
+    if os.path.isfile(path):
+        cnn = get_CNN_architecture(wnd_sz)
+        cnn.load_state_dict(torch.load(path))
+        cnn.eval()
+        return cnn
+    else:
+        return None
+
+def compute_output_dim(M,N,K0,K1,P0=0,P1=0,S0=1,S1=1,D0=1,D1=1):
+    HOUT = int((M + 2*P0 - D0*(K0-1)-1) / S0 +1)
+    WOUT = int((N + 2*P1 - D1*(K1-1)-1) / S1 +1)
+    return HOUT,WOUT
+
+class Net(nn.Module):
+    def __init__(self, wnd_sz):
+        super().__init__()
+        self.wnd_sz = wnd_sz
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        HOUT,WOUT = compute_output_dim(128,self.wnd_sz,5,5)
+        HOUT = int(HOUT / 2)
+        WOUT = int(WOUT / 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        HOUT2,WOUT2 = compute_output_dim(HOUT,WOUT,5,5)
+        HOUT2 = int(HOUT2 / 2)
+        WOUT2 = int(WOUT2 / 2)
+        self.fc1 = nn.Linear(16 * HOUT2 * WOUT2, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, np.prod(x.shape[1:]))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+def get_CNN_architecture(wnd_sz):
+    # - Create sequential model
+    cnn = Net(wnd_sz)
+    cnn = cnn.to(device)
+    return cnn
