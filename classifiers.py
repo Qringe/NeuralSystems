Remove second max pool layer, fix computation of max pool out dim

classifiers.py
@@ -364,15 +364,12 @@ class Net(nn.Module):
             self.input_sz = 2 * wnd_sz
         self.wnd_sz = wnd_sz
         self.conv1 = nn.Conv2d(1, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
         HOUT, WOUT = compute_output_dim(128, self.input_sz, 5, 5)
-        HOUT = int(HOUT / 2)
-        WOUT = int(WOUT / 2)
+        self.pool = nn.MaxPool2d(2, 2)
+        HOUT, WOUT = compute_output_dim(HOUT, WOUT, 2, 2, S0=2, S1=2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        HOUT2, WOUT2 = compute_output_dim(HOUT, WOUT, 5, 5)
-        HOUT2 = int(HOUT2 / 2)
-        WOUT2 = int(WOUT2 / 2)
-        self.fc1 = nn.Linear(16 * HOUT2 * WOUT2, 120)
+        HOUT, WOUT = compute_output_dim(HOUT, WOUT, 5, 5)
+        self.fc1 = nn.Linear(16 * HOUT * WOUT, 120)
         self.fc2 = nn.Linear(120, 84)
         # Changed 10 to 2
         self.fc3 = nn.Linear(84, 2)
@@ -383,7 +380,7 @@ class Net(nn.Module):
     def forward(self, x):
         x = x.to(DEVICE, dtype=torch.float)
         x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
+        x = F.relu(self.conv2(x))
         x = x.view(-1, np.prod(x.shape[1:]))
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))